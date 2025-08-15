2e0a9382b46affe707d18ab451bd6f92cb5e5aee
Juampa1907 <juampasemi19@gmail.com>
2025-08-15 09:03:49 -0500

    Primer cambio
    1 - creación atributo, constructores, getters y setters

--- Biografia.java
+++ Biografia.java
@@ -1,1 +1,34 @@
+package co.edu.unbosque.model;
 
+public class Biografia extends Publicacion {
+
+	private boolean esAutobiografia;
+
+	public Biografia() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Biografia(boolean esAutobiografia) {
+		super();
+		this.esAutobiografia = esAutobiografia;
+	}
+
+	public Biografia(String titulo, String autor, String formato, String idioma, int fechaDePublicacion,
+			boolean esAutobiografia) {
+		super(titulo, autor, formato, idioma, fechaDePublicacion);
+		this.esAutobiografia = esAutobiografia;
+	}
+
+	public Biografia(String titulo, String autor, String formato, String idioma, int fechaDePublicacion) {
+		super(titulo, autor, formato, idioma, fechaDePublicacion);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isEsAutobiografia() {
+		return esAutobiografia;
+	}
+
+	public void setEsAutobiografia(boolean esAutobiografia) {
+		this.esAutobiografia = esAutobiografia;
+	}
+}
