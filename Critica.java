253aabcefada3d319f467489834c78efd639072a
Jnk2206 <jcabarcasroso@gmail.com>
2025-08-16 15:55:49 -0500

    Update Critica.java

--- Critica.java
+++ Critica.java
@@ -4,6 +4,29 @@ public class Critica extends Publicacion {
 
 	private String queSeCritica;
 	private float calificacion;
+
+	public Critica() {
+		super();
+	}
+
+	public Critica(String queSeCritica, float calificacion) {
+		super();
+		this.queSeCritica = queSeCritica;
+		this.calificacion = calificacion;
+	}
+	public Critica(String titulo, String autor, String formato, String idioma, String fechaPublicacion,
+			boolean estaDisponible, String queSeCritica, float calificacion) {
+		super(titulo, autor, formato, idioma, fechaPublicacion, estaDisponible);
+		this.queSeCritica = queSeCritica;
+		this.calificacion = calificacion;
+	}
+	
+	public Critica(String titulo, String autor, String formato, String idioma, String fechaPublicacion,
+			boolean estaDisponible) {
+		super(titulo, autor, formato, idioma, fechaPublicacion, estaDisponible);
+		// TODO Auto-generated constructor stub
+	}
+	
 	public String getQueSeCritica() {
 		return queSeCritica;
 	}
@@ -16,25 +39,7 @@ public class Critica extends Publicacion {
 	public void setCalificacion(float calificacion) {
 		this.calificacion = calificacion;
 	}
-	public Critica(String queSeCritica, float calificacion) {
-		super();
-		this.queSeCritica = queSeCritica;
-		this.calificacion = calificacion;
-	}
-	public Critica(String titulo, String autor, String formato, String idioma, String fechaPublicacion,
-			boolean estaDisponible, String queSeCritica, float calificacion) {
-		super(titulo, autor, formato, idioma, fechaPublicacion, estaDisponible);
-		this.queSeCritica = queSeCritica;
-		this.calificacion = calificacion;
-	}
-	public Critica() {
-		super();
-	}
-	public Critica(String titulo, String autor, String formato, String idioma, String fechaPublicacion,
-			boolean estaDisponible) {
-		super(titulo, autor, formato, idioma, fechaPublicacion, estaDisponible);
-		// TODO Auto-generated constructor stub
-	}
+	
 	@Override
 	public String toString() {
 		return super.toString() + "Critica [queSeCritica=" + queSeCritica + ", calificacion=" + calificacion + "]";
