44dcbbbd6638f12ada9771020dac76b97ba35322
Jnk2206 <jcabarcasroso@gmail.com>
2025-08-16 15:54:31 -0500

    Update Libro.java

--- Libro.java
+++ Libro.java
@@ -5,6 +5,30 @@ public class Libro extends Publicacion {
 	private int numPaginas;
 	private String edicion;
 	private String prologo;
+	
+	public Libro() {
+		super();
+	}
+
+	public Libro(int numPaginas, String edicion, String prologo) {
+		super();
+		this.numPaginas = numPaginas;
+		this.edicion = edicion;
+		this.prologo = prologo;
+	}
+	public Libro(String titulo, String autor, String formato, String idioma, String fechaPublicacion,
+			boolean estaDisponible, int numPaginas, String edicion, String prologo) {
+		super(titulo, autor, formato, idioma, fechaPublicacion, estaDisponible);
+		this.numPaginas = numPaginas;
+		this.edicion = edicion;
+		this.prologo = prologo;
+	}
+	
+	public Libro(String titulo, String autor, String formato, String idioma, String fechaPublicacion,
+			boolean estaDisponible) {
+		super(titulo, autor, formato, idioma, fechaPublicacion, estaDisponible);
+		// TODO Auto-generated constructor stub
+	}
 	public int getNumPaginas() {
 		return numPaginas;
 	}
@@ -23,27 +47,7 @@ public class Libro extends Publicacion {
 	public void setPrologo(String prologo) {
 		this.prologo = prologo;
 	}
-	public Libro(int numPaginas, String edicion, String prologo) {
-		super();
-		this.numPaginas = numPaginas;
-		this.edicion = edicion;
-		this.prologo = prologo;
-	}
-	public Libro(String titulo, String autor, String formato, String idioma, String fechaPublicacion,
-			boolean estaDisponible, int numPaginas, String edicion, String prologo) {
-		super(titulo, autor, formato, idioma, fechaPublicacion, estaDisponible);
-		this.numPaginas = numPaginas;
-		this.edicion = edicion;
-		this.prologo = prologo;
-	}
-	public Libro() {
-		super();
-	}
-	public Libro(String titulo, String autor, String formato, String idioma, String fechaPublicacion,
-			boolean estaDisponible) {
-		super(titulo, autor, formato, idioma, fechaPublicacion, estaDisponible);
-		// TODO Auto-generated constructor stub
-	}
+	
 	@Override
 	public String toString() {
 		return super.toString() + "Libro [numPaginas=" + numPaginas + ", edicion=" + edicion + ", prologo=" + prologo + "]";
